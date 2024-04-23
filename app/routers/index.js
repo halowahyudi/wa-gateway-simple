7dfbcc7ef5e69a023684ce8c37df5554519eaf6e
Wahyudi <core@wahyudi.dev>
2024-04-23 18:45:18 +0800

    feat: main route

--- app/routers/index.js
+++ app/routers/index.js
@@ -4,7 +4,10 @@ const SessionRouter = require("./session_router");
 
 const MainRouter = Router();
 
+MainRouter.all("/", (req, res) => {
+  res.status(200).json({ Works: "fine!" });
+});
+
 MainRouter.use(SessionRouter);
 MainRouter.use(MessageRouter);
-
 module.exports = MainRouter;
